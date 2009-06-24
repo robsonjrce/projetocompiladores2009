1a3aaa6132fe26991b1fce979db0f93efe0c2ae5
robsonjrce <robsonjrce@415993ac-082e-11de-b753-7319d013a8fa>
2009-06-24 03:09:11 +0000

    versao compilada para acompanhamento

--- src/Mips/InReg.java
+++
@@ -1,1 +0,0 @@
-package Mips;import Temp.Temp;public class InReg extends Frame.Access{  Temp temp;  InReg(Temp t)  {    temp = t;  }  public Tree.Exp exp(Tree.Exp fp)  {    return new Tree.TEMP(temp);  }  public String toString()  {    return temp.toString();  }}
